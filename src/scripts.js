b8bef07a0c21fb286c1d61864924b134e59fdb23
Nathanael <dev@nathabonfim59.com>
2022-09-16 22:45:07 -0300

    ADD estrutura inicial do HTML

---
+++ src/scripts.js
@@ -0,0 +1,10 @@
+function loadPage(name) {
+    // TODO: Pegar o elemento #main
+    // TODO: Carregar o HTML correspondente
+    // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+
+    // NOTE: Passos
+    // 1. Carregar o HTML em uma variável
+    // 2. Pegar o elemento via ID (main)
+    // 3. Substituir o conteúdo do main pelo da variável
+}
