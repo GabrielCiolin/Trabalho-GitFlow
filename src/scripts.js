4ff41ce9f5125c912e63b01914253ad40c8b1fba
Nathanael <dev@nathabonfim59.com>
2022-09-17 19:26:07 -0300

    ADD troca de classe na página ativa

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,16 @@
-async function loadPage(name) {
+async function loadPage(event, name) {
     var main_content = document.getElementById("main");
     var response = await fetch(`paginas/${name}.html`);
     
     main_content.innerHTML = await response.text();
+
+    // Altera o elemento ativo para a página atual
+    if (!event) return;
+
+    var previousElement = document.getElementsByClassName('active')[0];
+    var currentElement = event.target;
+
+    previousElement.classList.remove('active');
+    currentElement.classList.add('active');
 }
-loadPage("principal");
+loadPage(null, "principal");
