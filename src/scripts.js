95676eb5719aa74a2278974531d14d15835c6d91
Yohann Gonçalves <36939172+BlackburnRX@users.noreply.github.com>
2022-09-17 00:22:20 -0300

    ADD script das paginas

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,10 @@
 function loadPage(name) {
     // TODO: Pegar o elemento #main
+    var main_content = document.getElementById("main");
+    var loader = new XMLHttpRequest();
+    // console.log(main_content.innerHTML);
+
+    // main_content.innerHTML = "principal";
     // TODO: Carregar o HTML correspondente
     // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
 
@@ -7,4 +12,6 @@ function loadPage(name) {
     // 1. Carregar o HTML em uma variável
     // 2. Pegar o elemento via ID (main)
     // 3. Substituir o conteúdo do main pelo da variável
+    
 }
+loadPage()
