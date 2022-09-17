21e36920b5c3c87ab1f8f2eb6745aad1c29cfd1e
Yohann Gonçalves <36939172+BlackburnRX@users.noreply.github.com>
2022-09-17 01:28:05 -0300

    ADD função para troca de página dinamica

--- src/scripts.js
+++ src/scripts.js
@@ -1,17 +1,7 @@
-function loadPage(name) {
-    // TODO: Pegar o elemento #main
+async function loadPage(name) {
     var main_content = document.getElementById("main");
-    var loader = new XMLHttpRequest();
-    // console.log(main_content.innerHTML);
-
-    // main_content.innerHTML = "principal";
-    // TODO: Carregar o HTML correspondente
-    // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
-
-    // NOTE: Passos
-    // 1. Carregar o HTML em uma variável
-    // 2. Pegar o elemento via ID (main)
-    // 3. Substituir o conteúdo do main pelo da variável
+    var response = await fetch(`paginas/${name}.html`);
     
+    main_content.innerHTML = await response.text();
 }
-loadPage()
+loadPage("principal");
